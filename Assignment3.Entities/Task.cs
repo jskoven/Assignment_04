3610d2e15b38c27278f7841d694da4d34cd9798d
Nicol <nihp>
2022-09-23 13:19:59 +0200

    task setup

--- Assignment3.Entities/Task.cs
+++ Assignment3.Entities/Task.cs
@@ -2,4 +2,21 @@ namespace Assignment3.Entities;
 
 public class Task
 {
+    int Id;
+    [StringLength(100)]
+    string Title; 
+    User AssignedTo;
+    string Description;
+    State state;
+    Tag[] tags;
+
+    enum State{
+        New,
+        Active,
+        Resolved,
+        Closed,
+        Removed
+
+    }
+
 }
