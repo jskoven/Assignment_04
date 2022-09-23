ee86f4de25e5eea5b610d5f67a51bc1b422f8871
alberted115 <aldy@itu.dk>
2022-09-23 13:43:57 +0200

    user and tag

--- Assignment3.Entities/Tag.cs
+++ Assignment3.Entities/Tag.cs
@@ -1,5 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment3.Entities;
 
 public class Tag
 {
+    private int? Id { get; set; }
+    [Key]
+    [StringLength(50)]
+    private string Name { get; set; }
+    private Task[]? Tasks { get; set; }
+
+    public Tag(string name)
+    {
+        Name = name;
+    }
 }
--- Assignment3.Entities/User.cs
+++ Assignment3.Entities/User.cs
@@ -1,5 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment3.Entities;
 
 public class User
 {
+    private int? Id { get; set; }
+    [StringLength(100)]
+    private string Name { get; set; }
+    [StringLength(100)]
+    [Key]
+    private string Email { get; set; }
+    private List<Task>? Tasks { get; set; }
+
+    public User(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
 }
